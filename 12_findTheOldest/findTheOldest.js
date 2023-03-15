c9125dea94ca36f4bf3a7d6c4d62a9b99d173df2
happy-balloon <product.rohini@gmail.com>
2023-03-15 15:28:33 +0100

    Completed Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arg) {
+    
+    const newArray = arg.map(v => {
+        if(!v.yearOfDeath) {
+            v.yearOfDeath = new Date().getFullYear()
+        }
+        v.age = v.yearOfDeath - v.yearOfBirth
+    });
 
+    console.log(arg)
+
+    const oldest = arg.sort((a, b) => b.age-a.age)
+        
+    console.log(oldest[0])
+
+    return oldest[0]
 };
 
 // Do not edit below this line
